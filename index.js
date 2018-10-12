ab0666dd5f2d20ef372164382c27aa7d5c22d7a1
Dirk Hornung <dirkhornung91@gmail.com>
2018-10-12 16:48:38 +0200

    axios createCustomer call

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('hello')
+const axios = require('axios')
+
+const saltedge = axios.create({
+  baseURL: 'https://www.saltedge.com/api/v4/',
+  timeout: 1000,
+  headers: {
+    'App-Id': '8cZAsydKcx5ToQiWf0wsnN8CZfd2W0dzHoOLNZMTmaY',
+    'Secret': 'on4ipqMGZ6x16FJtEakjYvar3GAAzmZMo-oSBxzYniI'
+  }
+})
+
+const createCustomer = async (identifier) => {
+  try {
+    const { data: { data }} =  await saltedge.post('customers', {
+      data: { identifier }
+    })
+    return data
+  }
+  catch(err) {
+    const { status, data: { error_class, error_message} } = err.response
+    throw { status, error_class, error_message }
+  }
+}
+
+createCustomer(1).then(res => console.log('res', res)).catch(err => console.error('err', err))
