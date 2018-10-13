8af7f9e0047535947c9f1ea42827cba76f7698de
Dirk Hornung <dirkhornung91@gmail.com>
2018-10-13 16:19:23 +0200

    createCustomer, createLogin

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const axios = require('axios')
+require('dotenv').config()
 
 const saltedge = axios.create({
   baseURL: 'https://www.saltedge.com/api/v4/',
   timeout: 1000,
   headers: {
-    'App-Id': '8cZAsydKcx5ToQiWf0wsnN8CZfd2W0dzHoOLNZMTmaY',
-    'Secret': 'on4ipqMGZ6x16FJtEakjYvar3GAAzmZMo-oSBxzYniI'
+    'App-Id': process.env.APP_ID,
+    'Secret': process.env.SECRET
   }
 })
 
@@ -22,4 +23,25 @@ const createCustomer = async (identifier) => {
   }
 }
 
-createCustomer(1).then(res => console.log('res', res)).catch(err => console.error('err', err))
+const createLogin = async (customerId, username, password) => {
+  try {
+    const { data: { data }} =  await saltedge.post('logins', {
+      data: {
+        customer_id: customerId,
+        country_code: 'ES',
+        provider_code: 'sabadell_es',
+        fetch_scopes: [ 'accounts', 'transactions' ],
+        credentials: { login: username, password },
+        daily_refresh: true
+      }
+    })
+    return data
+  }
+  catch(err) {
+    const { status, data: { error_class, error_message} } = err.response
+    throw { status, error_class, error_message }
+  }
+}
+
+// createCustomer(1).then(res => console.log('res', res)).catch(err => console.error('err', err))
+// createLogin(2452332, '', '').then(res => console.log('res', res)).catch(err => console.error('err', err))
